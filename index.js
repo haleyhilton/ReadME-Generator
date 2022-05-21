e3b6c6d8f0402a51a6f5dc2e14d5d29634a521ac
Tater <haleymariehilton@gmail.com>
2022-05-20 21:40:07 -0700

    was able to get pathing right to generated README location as well as clean up some of the prompts. Used the README generator to start my own README for this project.

---
+++ index.js
@@ -0,0 +1,57 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+
+// TODO: Create an array of questions for user input
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "author",
+            message: "What is the author's name?",
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project:",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What kind of license should your project have, if any?",
+            choices: ["APACHE 2.0", "BSD 3", "GPL 3.0", "MIT", "None"]
+        },
+        {
+            type: "input",
+            name: "installations",
+            message: "What command should be run to install dependencies?",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What does the user need to know about using the repo?",
+        },
+    ])
+
+questions()
+.then((data) =>
+        fs.writeFile("./newReadMe.md", generateMarkdown(data),
+        (err) =>
+            err ? console.error(err) : console.log('Commit logged!')))
